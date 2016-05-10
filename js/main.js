3dc90c638d180fd78dc31091fc0963e798b516b8
emoc11 <emoc11@free.fr>
2016-05-10 14:55:41 +0200

    begin teaser

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+
+	// $(".center p").bind("mouseover", function() {
+	// 	TweenMax.to($(".center .degrade_img"), 1, {width: "100%", x:0, ease:Quad.easeOut});
+	// });
+
+});
